4de69dce555564feca8ca81ed43d79fea4b5e928
bubanalesumit157 <bubanalesumit157@gmail.com>
2024-07-01 21:21:18 +0530

    day 1 of chai_js

---
+++ basics/data_conversion.js
@@ -0,0 +1,22 @@
+let score = true;
+console.log(typeof(score));
+console.log(score);
+
+let n = Number(score);
+console.log(typeof n);
+console.log(n);
+
+
+// string "33" converted into  number
+// but "33s" = NaN but type show number
+// true 1 f 0
+// undefined nan
+
+
+let b = "e";
+let bb= Boolean(b);
+console.log(bb);
+
+// 1 = T ,0 = F
+// "d" T
+//  "" = F
