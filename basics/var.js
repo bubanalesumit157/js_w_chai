324c7673f83f2399e9ce461ad321eb86d3052b9f
bubanalesumit157 <bubanalesumit157@gmail.com>
2024-07-01 21:06:57 +0530

    Create var.js

---
+++ basics/var.js
@@ -0,0 +1,30 @@
+// "use strict";  // treat all js code as newer version
+
+
+const accountId = 14443;
+let accEmail = "h@email.com";
+var accPwsd ;
+accountCity = "bgm";  // this wont work if we put use strict 
+
+//accountName  =3;
+
+console.log(accountId);
+console.log(accEmail);
+console.log(accountCity);
+console.table([accEmail,accPwsd,accountId,accountCity])
+
+
+let name ="sumit";
+let age = 20;
+let b = true;
+let s;  //undefined
+//  number = 2 to power 53
+// bigint
+// string " "
+// boolean
+// undefined
+// null => standalone value
+// symbol => to talk abt uniqueness
+// object => (type of null is object) 
+
+console.log(typeof(null));
